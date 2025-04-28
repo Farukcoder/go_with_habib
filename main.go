5a2e8f32a2f5a6ccc5b3ae7c765ee9538ebf8cd8
farukcoder <akomarfci@gmail.com>
2025-04-28 23:24:07 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("hellow world")
+
+	/*var x int = 10
+	fmt.Println(x)*/
+
+	// a := 10
+	// b := 10.02
+	// c := "hello world"
+	// d := true
+	// d = false
+
+	const p = 10
+
+	// fmt.Println(a, b, c, d)
+	fmt.Println(p)
+}
