d046cecdf0ee3265dd50f9ef0a8dafed3655d6bb
farukcoder <akomarfci@gmail.com>
2025-04-28 23:43:08 +0600

    if else and switch case done

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	// fmt.Println("hellow world")
 
+//variable and data type
+
 	/*var x int = 10
 	fmt.Println(x)*/
 
@@ -14,8 +16,52 @@ func main() {
 	// d := true
 	// d = false
 
-	const p = 10
+	//const p = 10
 
 	// fmt.Println(a, b, c, d)
-	fmt.Println(p)
+	// fmt.Println(p)
+
+//Condition if else & switch case
+
+	// age := 18
+
+	// if age > 18 {
+	// 	fmt.Println("You are eligible to be married")
+	// } else if age < 18 {
+	// 	fmt.Println("You are not eligible to be married, but you can love someone")
+	// } else {
+	// 	fmt.Println("You are just a teenager, not eligible to be married")
+	// }
+
+	// age := 20
+
+	// sex := "male"
+
+	// if age > 60 || sex == "male" {
+	// 	fmt.Println("You are ready to married")
+	// }
+
+	// isPretty := false
+
+	// if !isPretty {
+	// 	fmt.Println("ok")
+	// }
+
+	// if isPretty {
+	// 	fmt.Println("ok")
+	// }
+
+	// age := 3
+
+	// switch age {
+	// case 1:
+	// 	fmt.Println("age is 1")
+	// case 2,3 :
+	// 	fmt.Println("age is 2 or 3")
+	// default:
+	// 	fmt.Println("age is either 1 nor 2 or 3")
+	// }
+
+// function
+
 }
