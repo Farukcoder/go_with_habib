703741df81712f51b0778be0ce5450f426b6c11c
Md Omar faruk <165884633+smartThinkFaruk@users.noreply.github.com>
2025-06-18 17:26:40 +0600

    pointer done

--- pointer.go
+++ pointer.go
@@ -2,8 +2,32 @@ package main
 
 import "fmt"
 
+//func print(numbers *[3]int) {
+//	fmt.Println(numbers)
+//}
+
+type User struct {
+	Name   string
+	Age    int
+	Salary float64
+	foods  []string
+}
+
 func main() {
-	var x = 20
-	addr := &x
-	fmt.Println(x, addr)
+	//var x = 20
+	//fmt.Println(x)
+	//addr := &x
+	//*addr = 30
+	//fmt.Println(x, addr)
+	//fmt.Println(*addr)
+	//fmt.Println(x)
+	//arr := [3]int{1, 2, 3}
+	//print(&arr)
+	userInfo := User{
+		Name:   "Faruk",
+		Age:    20,
+		Salary: 100000,
+	}
+	pointer := &userInfo
+	fmt.Println(pointer.Name)
 }
